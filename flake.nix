9596bab52ff8973dbf778a03d533356fdd2ea28d
Markus John <johm@quyo.de>
2022-07-08 14:15:47 +0200

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,36 @@
 {
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "mersenneforumorg-flake";
-      config = { };
-      overlay = ./overlay.nix;
-      shell = ./shell.nix;
-    };
+    {
+      overlay = import ./overlay.nix;
+    }
+    //
+    flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
+      let
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay
+          ];
+        };
+
+      in {
+
+        packages = with pkgs; {
+          inherit gmp ecm-git;
+          inherit msieve-svn ytools ysieve yafu ggnfs;
+          inherit ecmpy factmsievepy aliqueit;
+          inherit cado-nfs;
+          inherit primesieve primecount primesum;
+        };
+
+      }
+    );
+
 }
--- overlay.nix
+++ overlay.nix
@@ -1,35 +1,22 @@
 self: super:
 
-let
-  nixpkgs = super;
-  allPkgs = nixpkgs // pkgs;
+with self; {
 
-  callPackage = path: overrides:
-    let f = import path;
-    in nixpkgs.lib.makeOverridable f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
+  ecm-git = callPackage ./packages/ecm-git { };
 
-  pkgs = rec {
-    gmp = nixpkgs.gmp;
-    ecm-git = callPackage ./packages/ecm-git { };
+  msieve-svn = callPackage ./packages/msieve-svn { ecm = ecm-git; zlib = null; };
+  ytools = callPackage ./packages/ytools { };
+  ysieve = callPackage ./packages/ysieve { };
+  yafu = callPackage ./packages/yafu { ecm = ecm-git; msieve = msieve-svn; };
+  ggnfs = callPackage ./packages/ggnfs { };
+  ecmpy = callPackage ./packages/ecmpy { ecm = ecm-git; };
+  factmsievepy = callPackage ./packages/factmsievepy { msieve = msieve-svn; };
+  aliqueit = callPackage ./packages/aliqueit { ecm = ecm-git; msieve = msieve-svn; };
 
-    msieve-svn = callPackage ./packages/msieve-svn { ecm = ecm-git; zlib = null; };
-    ytools = callPackage ./packages/ytools { };
-    ysieve = callPackage ./packages/ysieve { };
-    yafu = callPackage ./packages/yafu { ecm = ecm-git; msieve = msieve-svn; };
-    ggnfs = callPackage ./packages/ggnfs { };
-    ecmpy = callPackage ./packages/ecmpy { ecm = ecm-git; };
-    factmsievepy = callPackage ./packages/factmsievepy { msieve = msieve-svn; };
-    aliqueit = callPackage ./packages/aliqueit { ecm = ecm-git; msieve = msieve-svn; };
+  cado-nfs = callPackage ./packages/cado-nfs { ecm = ecm-git; };
 
-    cado-nfs = callPackage ./packages/cado-nfs { ecm = ecm-git; };
+  primesieve = callPackage ./packages/primesieve { };
+  primecount = callPackage ./packages/primecount { };
+  primesum = callPackage ./packages/primesum { };
 
-    primesieve = callPackage ./packages/primesieve { };
-    primecount = callPackage ./packages/primecount { };
-    primesum = callPackage ./packages/primesum { };
-  };
-in
-
-{
-  # this key should be the same as the flake name attribute.
-  mersenneforumorg-flake = pkgs;
 }
