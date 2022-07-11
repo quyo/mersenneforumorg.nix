0ddba2622c9d26e535bd24e9384936238976b804
Markus John <johm@quyo.de>
2022-07-11 21:17:01 +0200

    Add unwrapped apps

--- apps.nix
+++ apps.nix
@@ -5,18 +5,20 @@ let
   pkgs = self.packages.${system};
 
   apps = with pkgs; {
-    aliqueit        = { type = "app"; program = "${pkgs.aliqueit}/bin/aliqueit"; };
-    cado-nfs        = { type = "app"; program = "${pkgs.cado-nfs}/bin/cado-nfs.py"; };
-    cado-nfs-client = { type = "app"; program = "${pkgs.cado-nfs}/bin/cado-nfs-client.py"; };
-    ecm             = { type = "app"; program = "${pkgs.ecm-git}/bin/ecm"; };
-    ecmpy           = { type = "app"; program = "${pkgs.ecmpy}/bin/ecm.py"; };
-    factmsievepy    = { type = "app"; program = "${pkgs.factmsievepy}/bin/factmsieve.py"; };
-    msieve          = { type = "app"; program = "${pkgs.msieve-svn}/bin/msieve"; };
-    primecount      = { type = "app"; program = "${pkgs.primecount}/bin/primecount"; };
-    primesieve      = { type = "app"; program = "${pkgs.primesieve}/bin/primesieve"; };
-    primesum        = { type = "app"; program = "${pkgs.primesum}/bin/primesum"; };
-    yafu            = { type = "app"; program = "${pkgs.yafu}/bin/yafu"; };
-    ysieve          = { type = "app"; program = "${pkgs.ysieve}/bin/ysieve"; };
+    aliqueit           = { type = "app"; program = "${pkgs.aliqueit}/bin/aliqueit"; };
+    aliqueit-unwrapped = { type = "app"; program = "${pkgs.aliqueit-unwrapped}/bin/aliqueit"; };
+    cado-nfs           = { type = "app"; program = "${pkgs.cado-nfs}/bin/cado-nfs.py"; };
+    cado-nfs-client    = { type = "app"; program = "${pkgs.cado-nfs}/bin/cado-nfs-client.py"; };
+    ecm                = { type = "app"; program = "${pkgs.ecm-git}/bin/ecm"; };
+    ecmpy              = { type = "app"; program = "${pkgs.ecmpy}/bin/ecm.py"; };
+    factmsievepy       = { type = "app"; program = "${pkgs.factmsievepy}/bin/factmsieve.py"; };
+    msieve             = { type = "app"; program = "${pkgs.msieve-svn}/bin/msieve"; };
+    primecount         = { type = "app"; program = "${pkgs.primecount}/bin/primecount"; };
+    primesieve         = { type = "app"; program = "${pkgs.primesieve}/bin/primesieve"; };
+    primesum           = { type = "app"; program = "${pkgs.primesum}/bin/primesum"; };
+    yafu               = { type = "app"; program = "${pkgs.yafu}/bin/yafu"; };
+    yafu-unwrapped     = { type = "app"; program = "${pkgs.yafu-unwrapped}/bin/yafu"; };
+    ysieve             = { type = "app"; program = "${pkgs.ysieve}/bin/ysieve"; };
   };
 
 in
